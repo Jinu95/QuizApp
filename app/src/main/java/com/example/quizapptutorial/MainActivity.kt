7a9c8da587dd536f8d161fb14b8f59c7bf273d02
Jinwoo Lee <81419189+Jinu95@users.noreply.github.com>
2022-11-27 16:39:38 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/quizapptutorial/MainActivity.kt
+++ app/src/main/java/com/example/quizapptutorial/MainActivity.kt
@@ -12,18 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonStart: Button = findViewById(R.id.btn_start)
-        val etName: AppCompatEditText = findViewById(R.id.et_name)
+        val startButton: Button = findViewById(R.id.btn_start)
+        val nameEditText: AppCompatEditText = findViewById(R.id.et_name)
 
         buttonStart.setOnClickListener {
-            if (etName.text.toString().isEmpty()){
-                Toast.makeText(this,"Please Enter Your Name",Toast.LENGTH_SHORT).show()
-            }else{
-                val intent = Intent(this,QuizQuestionsActivity::class.java)
-                intent.putExtra(Constants.USER_NAME, etName.text.toString())
+            etName.text?.let {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, it.toString())
                 startActivity(intent)
                 finish()
-            }
+            } ?: Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT).show()
         }
     }
 }
